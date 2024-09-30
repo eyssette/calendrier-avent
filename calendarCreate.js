b91d1abc84e066b88b74c805954ed521c6b5d594
eyssette <cedric.eyssette@gmail.com>
2024-09-30 09:32:50 +0200

    liens vers forge sur apps

--- calendarCreate.js
+++ calendarCreate.js
@@ -5,8 +5,8 @@ const defaultCalendar = `# Mon calendrier de l'Avent
 > 
 > 1. Créez un fichier sur CodiMD ou sur une forge.
 > 2. Ce fichier doit comporter : un titre, un bloc de citation qui constituera le message initial, ainsi que pour chaque jour un titre, au moins une image et du contenu qui s'affichera quand on clique dessus. Vous pouvez récupérer [ce modèle](https://codimd.apps.education.fr/-PbI3GizQo6xV-TEiU1-sA?both). 
-> 3. Votre calendrier de l'avent sera alors disponible à l'adresse : https://eyssette.forge.aeif.fr/calendrier-avent#URL (en remplaçant URL par l'URL de votre fichier).
-> 4. Il est possible de cacher l'URL de votre fichier avec : https://eyssette.forge.aeif.fr/calendrier-avent?c=1#URL (l'URL doit être encodée avec base64)
+> 3. Votre calendrier de l'avent sera alors disponible à l'adresse : https://eyssette.forge.apps.education.fr/calendrier-avent#URL (en remplaçant URL par l'URL de votre fichier).
+> 4. Il est possible de cacher l'URL de votre fichier avec : https://eyssette.forge.apps.education.fr/calendrier-avent?c=1#URL (l'URL doit être encodée avec base64)
 
 ## 1
 ![](img/johnny_automatic_Christmas_Tree_1.png) ![](https://picsum.photos/200?random=1)
